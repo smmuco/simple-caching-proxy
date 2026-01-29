8013c6f297b0be14ae2dafdf208ff29ecd9bd670
Mücahit Sahin <smucahit333@gmail.com>
2026-01-29 14:41:59 +0300

    feat: add validation for port and origin argument

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿
-
 string? port = null;
-string? orgin = null;
+string? origin = null;
 
 
 for (int i = 0; i<args.Length; i++)
@@ -12,11 +11,33 @@ for (int i = 0; i<args.Length; i++)
     }
     else if(args[i] == "--origin" && i + 1 < args.Length)
     {
-        orgin = args[i + 1];
+        origin = args[i + 1];
     }
 }
+
+
+if (!int.TryParse(port, out int portNumber))
+{
+    Console.WriteLine("Error: port must be a number");
+    Environment.Exit(1);
+}
+
+if (portNumber<1 || portNumber>65535)
+{
+    Console.WriteLine("Error: port must be between 1 and 65535");
+    Environment.Exit(1);
+}
+
+if (!Uri.TryCreate(origin,UriKind.Absolute, out Uri? originUri))
+{
+    Console.WriteLine("Error: origin must be a valid absolute URL");
+    Environment.Exit(1);
+}
+
+
+
 Console.WriteLine($"Port: {port}");
-Console.WriteLine($"Orgin: {orgin}");
+Console.WriteLine($"Orgin: {origin}");
 
 
 
