e9e7e791ed085c348aebd8f15b5fb1765c6356c7
Mücahit Sahin <smucahit333@gmail.com>
2026-01-29 14:28:27 +0300

    feat: parse --port and --origin from args

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
 ﻿
 
+string? port = null;
+string? orgin = null;
+
+
+for (int i = 0; i<args.Length; i++)
+{
+    if(args[i] == "--port" && i + 1 < args.Length)
+    {
+        port = args[i + 1];
+    }
+    else if(args[i] == "--origin" && i + 1 < args.Length)
+    {
+        orgin = args[i + 1];
+    }
+}
+Console.WriteLine($"Port: {port}");
+Console.WriteLine($"Orgin: {orgin}");
+
 
 
